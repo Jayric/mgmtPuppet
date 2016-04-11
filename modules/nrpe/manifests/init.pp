9b1a6960f185fae5f55d8192f9b83a70cf63ec5b
hornoo <rchrdhorne1@gmail.com>
2016-04-12 10:10:57 +1200

    added nrpe install classes to nrpe puppet module

--- modules/nrpe/manifests/init.pp
+++ modules/nrpe/manifests/init.pp
@@ -2,8 +2,17 @@ class nrpe {
 	include nrpe::install, nrpe::config, nrpe::service
         
         if $hostname == 'db'{
+		
            include db_nrpe_config
         }
+		elsif $hostname == 'app'
+		{
+			include app_nrpe_config
+			
+		} elsif $hostname == 'storage'
+		{
+			include storage_nrpe_config
+		}
         
 }
 
@@ -13,6 +22,24 @@ class nrpe::db_nrpe_config{
 	owner => 'root',
 	group => 'root',
 	mode => 0644,
-	content => template('nrpe/dbnrpeconfig.erb'),
+	content => template('nrpe/db_nrpeconfig.erb'),
+	}
+	
+	class nrpe::storage_nrpe_config{
+	file{"/etc/nagios/nrpe.cfg" :
+	ensure => present,
+	owner => 'root',
+	group => 'root',
+	mode => 0644,
+	content => template('nrpe/storage_nrpeconfig.erb'),
+	}
+	
+	class nrpe::app_nrpe_config{
+	file{"/etc/nagios/nrpe.cfg" :
+	ensure => present,
+	owner => 'root',
+	group => 'root',
+	mode => 0644,
+	content => template('nrpe/app_nrpeconfig.erb'),
 	}
 }	
