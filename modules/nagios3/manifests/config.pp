e975fa620882e27bea821958b77629699fee4822
root <root@vCloud.Debian75>
2016-04-13 15:10:49 +1200

    added memeory monitor config

--- modules/nagios3/manifests/config.pp
+++ modules/nagios3/manifests/config.pp
@@ -169,7 +169,21 @@ class nagios3::config{
 	notification_options => 'w,u,c',
 	contact_groups => 'sysadmins',
 	}
-	
+
+        nagios_service {'Remote-Memory':
+        service_description => 'Check for Memory',
+        hostgroup_name => 'linux-Standard-Monitor',
+        target => '/etc/nagios3/conf.d/ppt_Check_Memory.cfg', 
+	check_command => 'check_nrpe_1arg!check_mem',
+        max_check_attempts => 3,
+	retry_check_interval => 1,
+	normal_check_interval => 5,
+	check_period => '24x7',
+	notification_interval => 30,
+	notification_period => '24x7',
+	notification_options => 'w,u,c',
+	contact_groups => 'sysadmins',
+	}
 	
 
 	nagios_hostgroup{'db-servers':
